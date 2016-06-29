f2786bcebcab65b76a3613429b1fb7255ceeaf74
Shiva <mail2shiva.p17@gmail.com>
2016-06-30 01:54:01 +0530

    Added basic template and required js and css libraries

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- All required libraries -->
+<script type="text/javascript" src="{{ asset('public/bower/jquery/jquery.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('public/bower/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('public/bower/underscore/underscore.js') }}"></script>
+
+<!-- All required Directives -->
+
+</html>
+
